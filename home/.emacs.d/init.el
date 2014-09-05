2cebe4d6f57694fc059ba96347f1ed6edd5f4507
Jake McCrary <jake@jakemccrary.com>
2014-09-05 08:43:43 -0500

    company mode everywhere

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -193,38 +193,39 @@
 ;; killing things.
 (setq cider-repl-print-length 100)
 
+;; Company mode all over.
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; auto-complete configuration
-(require 'popup)
-(require 'fuzzy)
-(require 'auto-complete)
-(require 'auto-complete-config)
-(ac-config-default)
-(ac-flyspell-workaround)
-(setq ac-comphist-file (concat tmp-dir "ac-comphist.dat"))
-(global-auto-complete-mode t)
-(setq ac-auto-show-menu t)
-(setq ac-dwim t)
-(setq ac-quick-help-delay 1)
-(setq ac-quick-help-height 60)
-(setq ac-disable-inline t)
-(setq ac-show-menu-immediately-on-auto-complete t)
-(setq ac-auto-start 2)
-(setq ac-candidate-menu-min 0)
+;; (require 'popup)
+;; (require 'fuzzy)
+;; (require 'auto-complete)
+;; (require 'auto-complete-config)
+;; (ac-config-default)
+;; (ac-flyspell-workaround)
+;; (setq ac-comphist-file (concat tmp-dir "ac-comphist.dat"))
+;; (global-auto-complete-mode t)
+;; (setq ac-auto-show-menu t)
+;; (setq ac-dwim t)
+;; (setq ac-quick-help-delay 1)
+;; (setq ac-quick-help-height 60)
+;; (setq ac-disable-inline t)
+;; (setq ac-show-menu-immediately-on-auto-complete t)
+;; (setq ac-auto-start 2)
+;; (setq ac-candidate-menu-min 0)
 
-(set-default 'ac-sources
-             '(ac-source-dictionary
-               ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers
-               ac-source-semantic))
+;; (set-default 'ac-sources
+;;              '(ac-source-dictionary
+;;                ac-source-words-in-buffer
+;;                ac-source-words-in-same-mode-buffers
+;;                ac-source-semantic))
 
 
 
-(dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
-                                    sass-mode yaml-mode csv-mode espresso-mode haskell-mode
-                                    html-mode nxml-mode sh-mode smarty-mode clojure-mode
-                                    lisp-mode textile-mode markdown-mode tuareg-mode))
-  (add-to-list 'ac-modes mode))
+;; (dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
+;;                                     sass-mode yaml-mode csv-mode espresso-mode haskell-mode
+;;                                     html-mode nxml-mode sh-mode smarty-mode lisp-mode textile-mode markdown-mode tuareg-mode))
+;;   (add-to-list 'ac-modes mode))
 
 ;; erc
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))

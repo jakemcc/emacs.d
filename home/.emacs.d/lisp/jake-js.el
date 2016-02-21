4801fd38d3fb2728166e793454deb189c07931bd
Jake McCrary <jake@jakemccrary.com>
2016-02-21 16:59:10 -0600

    get singlequotes working in javascript with paredit

--- home/.emacs.d/lisp/jake-js.el
+++ home/.emacs.d/lisp/jake-js.el
@@ -7,9 +7,26 @@
        '((lambda (endp delimiter) nil)))
   (paredit-mode 1))
 
+;; https://github.com/dandavison/paredit-c/blob/master/paredit-c.el#L46
+(defun paredit-c/singlequote (&optional n)
+  "Copied from `paredit-doublequote'"
+  (interactive "P")
+
+  (if (null paredit-mode) (insert "'")
+    (cond ((paredit-in-string-p)
+           (if (eq (cdr (paredit-string-start+end-points))
+                   (point))
+               (forward-char) ; We're on the closing quote.
+             (insert ?\\ ?\' )))
+          ((paredit-in-comment-p)
+           (insert ?\' ))
+          ((not (paredit-in-char-p))
+           (paredit-insert-pair n ?\' ?\' 'paredit-forward-for-quote)))))
+
 (eval-after-load 'js
   '(progn (define-key js-mode-map "{" 'paredit-open-curly)
           (define-key js-mode-map "}" 'paredit-close-curly-and-newline)
+          (define-key js-mode-map "'" 'paredit-c/singlequote)
           (add-hook 'js-mode-hook 'esk-paredit-nonlisp)
           (setq js-indent-level 2)
           ;; fixes problem with pretty function font-lock

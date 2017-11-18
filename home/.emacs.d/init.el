056726edc19ef72d132b66888cf69aa14cc17d4f
Jake McCrary <jake@jakemccrary.com>
2017-11-18 13:36:54 -0600

    configure some org-mode

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -177,6 +177,11 @@
   :init (add-hook 'clojure-mode-hook 'dumb-jump-mode)
   :config (setq dumb-jump-selector 'ivy))
 
+(use-package org
+  :ensure t
+  :config
+  (setq org-clock-into-drawer "CLOCKING"))
+
 (use-package projectile
   :ensure t
   :config
@@ -500,32 +505,32 @@
 
 
 ;; Python
-(use-package python
-  :commands python-mode
-  :config
-  (progn
-    ;; Python hackers like their lines to be 72 columns.
-    (set-fill-column 72))
-  :ensure t)
+;; (use-package python
+;;   :commands python-mode
+;;   :config
+;;   (progn
+;;     ;; Python hackers like their lines to be 72 columns.
+;;     (set-fill-column 72))
+;;   :ensure t)
 
 ;; jedi provides auto completion for Python programs. Depends on the
 ;; Python packages "jedi" and "epc" to be installed on the host
 ;; machine.
-(use-package jedi
-  :init
-  (progn
-    (setq jedi:complete-on-dot t)
-    (setq jedi:setup-keys t)
-    (add-hook 'python-mode-hook 'jedi:setup))
-  :ensure t)
+;; (use-package jedi
+;;   :init
+;;   (progn
+;;     (setq jedi:complete-on-dot t)
+;;     (setq jedi:setup-keys t)
+;;     (add-hook 'python-mode-hook 'jedi:setup))
+;;   :ensure t)
 
 ;; company-jedi wires up jedi to be a backend for the auto completion
 ;; library, company-mode.
-(use-package company-jedi
-  :init
-  (add-hook 'python-mode-hook
-            (lambda () (add-to-list 'company-backends 'company-jedi)))
-  :ensure t)
+;; (use-package company-jedi
+;;   :init
+;;   (add-hook 'python-mode-hook
+;;             (lambda () (add-to-list 'company-backends 'company-jedi)))
+;;   :ensure t)
 
 (use-package rainbow-mode
   :ensure t)
@@ -557,7 +562,7 @@ This command does the reverse of `fill-paragraph'."
     ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1bacdd5d24f187f273f488a23c977f26452dffbc82d4ac57250aa041f14159da" default)))
  '(package-selected-packages
    (quote
-    (dockerfile-mode emojify rainbow-mode epc use-package)))
+    (edit-indirect dockerfile-mode emojify rainbow-mode epc use-package)))
  '(safe-local-variable-values
    (quote
     ((clojure-indent-style . t)

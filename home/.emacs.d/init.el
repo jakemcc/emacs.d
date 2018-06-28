093a966b52dfcd6596325d8a8a01551df75a0f1c
Jake McCrary <jake@jakemccrary.com>
2018-06-28 11:40:26 -0500

    Use emacs 26.1 display-line-numbers

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -281,9 +281,6 @@
 ;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
 (add-hook 'after-make-frame-functions '--set-emoji-font)
 
-;; show line numbers
-(global-linum-mode t)
-
 ;; spaces instead of tabs
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
@@ -580,6 +577,7 @@ This command does the reverse of `fill-paragraph'."
  '(custom-safe-themes
    (quote
     ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1bacdd5d24f187f273f488a23c977f26452dffbc82d4ac57250aa041f14159da" default)))
+ '(display-line-numbers t)
  '(package-selected-packages
    (quote
     (edit-indirect dockerfile-mode emojify rainbow-mode epc use-package)))

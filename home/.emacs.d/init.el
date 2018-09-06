fd798785a1a3c5e57ab1f98af4461ab149a9d743
Jake McCrary <jake@jakemccrary.com>
2018-09-05 19:27:55 -0500

    set org-modules

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -239,6 +239,7 @@ From: https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.h
   :bind (("C-c a" . org-agenda)
          ("C-c c" . 'org-capture))
   :custom
+  (org-modules '(org-habit org-w3m org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail))
   (org-todo-keywords
    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
      (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")

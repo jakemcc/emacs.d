63dccfe2d8158a6791d6609a53ecc6992b1aba6b
Jake McCrary <jake@jakemccrary.com>
2023-08-22 11:03:42 -0500

    add function for switching between vertical and horizontal splits

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1062,6 +1062,32 @@ same directory as the org-buffer and insert a link to this file."
   ;;   (add-to-list 'cljr-magic-require-namespaces mapping t))
   )
 
+(defun jm/switch-window-split ()
+  "Toggle between horizontal and vertical split for two windows. Thanks ChatGPT"
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
 ;;TODO: this is not correct
 (defun jake/window-is-small? ()
   (let ((half-height (/ (window-total-height (frame-root-window)) 2)))
